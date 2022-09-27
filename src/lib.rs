5a1ebce678c539e9c3cec8241591ddd63d3fe1dc
李大伟 <ldw01441929@alibaba-inc.com>
2022-09-27 12:09:47 +0800

    迭代器

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,17 @@ pub struct Config {
 }
 
 impl Config {
-  pub fn new(args: &[String]) -> Result<Config, &'static str> {
-    if args.len() < 3 {
-      return Err("The length of args is less than 3");
-    }
-    let query = args[1].clone();
-    let filename = args[2].clone();
+  pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+    args.next();
+
+    let query = match args.next() {
+      Some(s) => s,
+      None => return Err("get query failed"),
+    };
+    let filename = match args.next() {
+      Some(s) => s,
+      None => return Err("get filename failed"),
+    };
     let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
     Ok(Config {
       query,
@@ -39,28 +44,18 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 fn search_case_sensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-  let mut results = Vec::new();
-
-  for line in contents.lines() {
-    if line.contains(query) {
-      results.push(line);
-    }
-  }
-
-  results
+  contents
+    .lines()
+    .filter(|line| line.contains(query))
+    .collect()
 }
 
 fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-  let mut results = Vec::new();
   let query = query.to_lowercase();
-
-  for line in contents.lines() {
-    if line.to_lowercase().contains(&query) {
-      results.push(line);
-    }
-  }
-
-  results
+  contents
+    .lines()
+    .filter(|line| line.to_lowercase().contains(&query))
+    .collect()
 }
 
 #[cfg(test)]
